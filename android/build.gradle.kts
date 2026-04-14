b311ca21e5ef5217a9db47da9b31146d7bbe610f
Otabek Ravshanov <otabekxoff@gmail.com>
2026-04-14 15:33:36 +0300

    new apk

--- android/build.gradle.kts
+++ android/build.gradle.kts
@@ -5,6 +5,22 @@ allprojects {
     }
 }
 
+// Configure Kotlin for all subprojects
+subprojects {
+    afterEvaluate {
+        if (project.hasProperty("android")) {
+            project.plugins.withType<org.jetbrains.kotlin.gradle.plugin.KotlinBasePluginWrapper> {
+                project.tasks.withType<org.jetbrains.kotlin.gradle.tasks.KotlinCompile> {
+                    compilerOptions {
+                        apiVersion.set(org.jetbrains.kotlin.gradle.dsl.KotlinVersion.KOTLIN_1_9)
+                        languageVersion.set(org.jetbrains.kotlin.gradle.dsl.KotlinVersion.KOTLIN_1_9)
+                    }
+                }
+            }
+        }
+    }
+}
+
 val newBuildDir: Directory =
     rootProject.layout.buildDirectory
         .dir("../../build")
